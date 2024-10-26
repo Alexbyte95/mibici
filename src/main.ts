c2c84820de6950ac13a34cd7e4b2bc34d32a1d67
Alexis Bautista <svedra95@gmail.com>
2024-10-26 14:45:23 -0600

    feat: add DB Dependencies and configuration module and main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvVariablesService } from './Shared/domain/config/env-variables.service';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+  });
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(
+    new LoggerErrorInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({ forbidNonWhitelisted: true, stopAtFirstError: true }),
+  );
+
+  const configService = app.get(EnvVariablesService);
+  const appPort = configService.AppPort;
+  await app.listen(appPort);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.error(error);
+  process.exit(1);
+});
